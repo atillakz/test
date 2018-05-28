2f05a38b6ae910a471b8498b27b0fbb3b3d35ddc
Rustem <rustem.kussaiynov@nu.edu.kz>
2018-05-28 21:47:55 +0600

    Test commit

--- app/src/main/java/com/example/q/firstapp/MainActivity.java
+++ app/src/main/java/com/example/q/firstapp/MainActivity.java
@@ -38,6 +38,7 @@ public class MainActivity extends AppCompatActivity {
             startActivity(intent);
         });
 
+        // test new branch
     }
 
     public void increment(View view) {
